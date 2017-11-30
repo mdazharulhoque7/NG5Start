1dd3c49f6633e241560c8801102e874d966d921e
Md. Azharul Hoque <azharul.hoque@televersant.com>
2017-11-30 19:46:56 +0600

    initial commit

---
+++ src/app/_nav.ts
@@ -0,0 +1,15 @@
+export const navigation = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'icon-speedometer',
+    badge: {
+      variant: 'info',
+      text: 'NEW'
+    }
+  },
+  {
+    title: true,
+    name: 'Settings'
+  },  
+];
